89149e707806d4a880b065785761ae56acd23cfc
Fotis Spatharakis <66828845+fspathara@users.noreply.github.com>
2022-10-18 20:18:28 +0300

    first commit

---
+++ BitcoinPricePresenter/BitcoinPricePresenter/Controllers/BitcoinPriceController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BitcoinPricePresenter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BitcoinPriceController : ControllerBase
+    {
+    }
+}
